ed0f74393b225aa392d4ea395f4e8d09e9645fa6
♛ Oº°‘¨¤✘ ʞıǝɥs♕ Oº° 闕威廉 °ºO < O s O  O>
2016-10-06 17:04:48 -0700

    changed to jawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,10 @@
 Here is where you make the connection to the database and export and used by the O.R.M.
 */
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: 'uclabootcamp',
-    database: 'cars_db'
-});
-
+var connection;
+connection = mysql.createConnection(process.env.JAWSDB_URL);
 connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
 });
-
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE cars_db;
 USE cars_db;
 
-CREATE TABLE rental
+CREATE TABLE cars
 (
 	id int NOT NULL AUTO_INCREMENT,
 	model varchar(255) NOT NULL,
